12ecba803820cf4f6f6ae99d3930ae36145e25f7
Juan <juancambronerofresco@gmail.com>
2025-09-17 17:10:38 +0200

    metodos basicos de la clase inventario

--- src/Inventario.java
+++ src/Inventario.java
@@ -1,8 +1,15 @@
 import java.util.HashMap;
-
 public class Inventario {
     //Atributos
     private String nombreTienda;
+    HashMap<String,Productos> inventarioProductos;
 
+    //Constructor
+    public Inventario(String nombreTienda, HashMap<String, Productos> inventarioProductos) { this.nombreTienda = nombreTienda; this.inventarioProductos = inventarioProductos; }
     //Metodos
-}
+    //Agregar producto
+    public void agregarProducto(Productos producto) { inventarioProductos.put(producto.getCodProducto(), producto); }
+    //Eliminar producto
+    public void eliminarProducto(Productos producto) { inventarioProductos.remove(producto.getCodProducto()); }
+    //Actualizar stock
+    public void actualizarStock(Productos producto) { inventarioProductos.put(producto.getCodProducto(), producto); } }
